c7c5a8d61699e5d588ac170d8ec3e1fb89b7c1bc
Kamil73 <kamil.bylinka9@gmail.com>
2023-07-10 22:16:28 +0100

    first commit

---
+++ bytes_examples.py
@@ -0,0 +1,35 @@
+
+text = b'Hello World'
+print(text)
+# b'Hello World'
+
+text_2 = b'\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64'
+print(text_2)
+# b'Hello World'
+
+print(text == text_2)
+True
+
+def transform_1(text):
+    return [x for x in text]
+print(transform_1(text))
+# 72 101 108 108 111 32 87 111 114 108 100
+
+def transform_2(text):
+    return [hex(x) for x in text]
+print(transform_2(text))
+# 0x48 0x65 0x6c 0x6c 0x6f 0x20 0x57 0x6f 0x72 0x6c 0x64
+      
+def transform_3(text):
+    return [int(hex(x), 16) for x in text]
+print(transform_3(text))
+# 72 101 108 108 111 32 87 111 114 108 100
+
+def transform_4(text):
+    return [bytes.fromhex(hex(x)[2:]) for x in text]
+print(transform_4(text))
+# b'H' b'e' b'l' b'l' b'o' b' ' b'W' b'o' b'r' b'l' b'd'
+
+print('=' * 100)
+
+
